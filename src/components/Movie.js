f7ccbc54a8b87ee9ac63c6c9de2b768a96638a94
Limgabi <gabi520@naver.com>
2022-05-28 22:01:49 +0900

    using useParams

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,27 +1,29 @@
-import PropTypes from 'prop-types'; 
-import { useNavigate } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
-    let navigate = useNavigate();
-    return (
-        <div>
-            <img src={coverImg} alt={title}/>
-            <h2 onClick={() => navigate('/movie')}>{title}</h2>
-            <p>{summary}</p>
-            <ul>
-                {genres.map((g, idx) => (
-                    <li key={idx}>{g}</li>
-                ))}
-            </ul>
-        </div>
-    )
+function Movie({ id, coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g, idx) => (
+          <li key={idx}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 Movie.propTypes = {
-    coverImg: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,34 @@
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movieInfo, setMovieInfo] = useState([]);
+
+  const getMovie = useCallback(async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovieInfo(json.data.movie)
+    console.log(movieInfo)
+    setLoading(false);
+  }, [id])
+  useEffect(() => {
+    getMovie();
+  }, [getMovie])
+
   return (
-      <div>Detail</div>
+    <div>
+      {
+        loading
+          ? <h1>Loading...</h1>
+          : <div>
+            <img src={movieInfo.medium_cover_image}/>
+            <p>{movieInfo.title}</p>
+          </div>
+      }
+    </div>
   )
 }
 
