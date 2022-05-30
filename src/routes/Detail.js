4b578e1dffe446537f93abf695efac38749427b1
Limgabi <gabi520@naver.com>
2022-05-30 15:37:11 +0900

    detail page css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail() {
   const { id } = useParams();
@@ -11,6 +12,7 @@ function Detail() {
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     setMovieInfo(json.data.movie)
+    console.log(movieInfo)
     setLoading(false);
   })
   useEffect(() => {
@@ -23,8 +25,25 @@ function Detail() {
         loading
           ? <h1>Loading...</h1>
           : <div>
-            <img src={movieInfo.medium_cover_image}/>
-            <p>{movieInfo.title}</p>
+            <img className={styles.movie_background_img} src={movieInfo.background_image} />
+            <div className={styles.movie}>
+              <img src={movieInfo.medium_cover_image}/>
+              <div className={styles.movie_info}>
+                <h1>{movieInfo.title}</h1>
+                <ul style={{padding: "0 0 0 20px", fontSize: "18px"}}>
+                  <li>Rating {movieInfo.rating}</li>
+                  <li>Runtime {movieInfo.runtime}</li>
+                  <li>Download {movieInfo.download_count}</li>
+                  <li>Genres
+                    <ul style={{padding: "0 0 0 20px", fontSize: "16px"}}>
+                      {
+                        movieInfo.genres.map((g) => <li>{g}</li>)
+                      }
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
       }
     </div>
